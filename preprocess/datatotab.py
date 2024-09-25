3879ddb793ecac4aa66d9e0e167489f35cfd95b1
Rahul <80764087+rahulsk88@users.noreply.github.com>
2024-09-25 16:05:56 -0400

    Made some EDA. Still pending. Not clean

--- preprocess/datatotab.py
+++ preprocess/datatotab.py
@@ -1,100 +1,70 @@
 import os
-
 import json
 import pandas as pd
-import numpy as np
 
-## I'll add some multiprocessing once I know it works so I can save the dataset really really really quickly
 
-def json_to_tab(file_path):
+## Need to make it make a different column for each frame instead of having a different row. 
+
+## This would mean we have 125 * 240 rows of data each with 90-ish columns
+def json_to_tab(file_path, start_dict = None):
     """
-    Func for converting the Json data into csv file for easy 
-    data and sta analysis
-
-    Args: 
-        file_path (str): The location of the json file
-    
-    Returns:
-        tab (list): a list of the data
-        shot_id (int): the shot number from 1 to 125 
-    """
-
-    data_list = []
-    with open(file_path, 'r') as file:
-        data = json.load(file)
-
-        ## data is a dictionary cotaining 
-    keys = data.keys()
-    for i in keys:
-        if i != "tracking":
-            data_list.append(data[i])
-
-    for i in data['tracking']:
-        #frame_id = str(i['frame']) # I don't need this now. I need to return this later. Although it could be more effective to save
-        ## Not including time because there is a easy 1 to 1 correspondance so we can always get it back
-
-        data_list.extend(i['data']['ball'])
-        for j in i['data']['player'].keys():
-            data_list.extend(i['data']['player'][j])
-
-    ## list should have a size of 6 + 240*3*27 + 240*3
-    return data_list
-
-def convert_to_tab(json_dir):
-    """
-    This func does 2 main things. Call json_to_tab with the respective json paths. 
-    Then along with column names returns a dataframe that can be saved as a .csv file
+    func takes the json data and then returns a dict with 90 keys with each key having exactly 240 values
 
     Args:
-        json_dir: (str) the folder directory containing all the data that needs to be converted (may be adapted to include additional players)
-    
+        file_path (str): Pretty obvious
+        start_dict (dict): This is the dict that we append more data to. This could be empty #TODO: Add functionality for start_dict empty
     Returns:
-        dataset: (pd.dataset) This is the dataset with 125 rows 201-ish rows.
-    """ 
-    data_index = {}
-    columns = []
+        start_dict (dict): start_dict and then for each key append info to it.  (Index is participant id-trial number id-frame id) 
+    """
 
-    for index, filename in enumerate(os.listdir(json_dir)):
-        f = os.path.join(json_dir, filename)
+    if start_dict == None:
+        start_dict = {}
 
-        data_index[index] = json_to_tab(f)
-
-    with open(f, 'r') as file:
+    with open(file_path, 'r') as file:
         data = json.load(file)
     
-    columns.extend(data.keys())
-    columns.pop() ## scary
+    participant_id = data["participant_id"]
+    trial_id = data["trial_id"]
+    result = data["result"]
+    landing_x = data["landing_x"]
+    landing_y = data["landing_y"]
+    entry_angle = data["entry_angle"]
 
-    for i in data['tracking']:
-        frame_id = str(i['frame'])
-        for index_j, j in enumerate(i['data']['ball']):
-            if index_j == 0:
-                columns.append(f'{frame_id}_ball_x')
-            elif index_j == 1:
-                columns.append(f'{frame_id}_ball_y')
-            elif index_j == 2:
-                columns.append(f'{frame_id}_ball_z')
+    for i in data["tracking"]:
+        dict_list = []
+        dict_key = f"{participant_id}-{trial_id}-{i["frame"]}"
+        dict_list.extend([participant_id, trial_id, result, landing_x, landing_y, entry_angle])
+        dict_list.extend(i["data"]["ball"])
         
-        for k in i['data']['player'].keys():
-            for index_l, l in enumerate(i['data']['player'][k]):
-                if index_l == 0:
-                    columns.append(f'{frame_id}_{k}_x')
-                elif index_l == 1:
-                    columns.append(f'{frame_id}_{k}_y')
-                elif index_l == 2:
-                    columns.append(f'{frame_id}_{k}_z')
+        for _, j in i["data"]['player'].items():
+            dict_list.extend(j) 
+        start_dict[dict_key] = dict_list
 
-    data_csv = pd.DataFrame.from_dict(data_index, orient = 'index', columns = columns)
+    return start_dict  
+
+def convert_to_tab(json_dir):
+
+    dict_output = {}
+    columns = []
     
-    data_csv.to_csv("P0001_shooting.csv")
+    for _, filename in enumerate(os.listdir(json_dir)):
+        file_path = os.path.join(json_dir, filename)
+        if not filename.endswith('.json'):
+            continue
+        dict_output = json_to_tab(file_path, dict_output)
 
+    first_file_path = os.path.join(json_dir, os.listdir(json_dir)[0])    
+    with open(first_file_path, 'r') as file:
+        first_file_data = json.load(file)
+    columns.extend([key for key in first_file_data.keys() if key != 'tracking'])
+    
+    for frame in first_file_data['tracking']:
+        columns.extend(['ball_x', 'ball_y', 'ball_z'])
+        for player_id in frame['data']['player'].keys():
+            columns.extend([f'{player_id}_x', f'{player_id}_y', f'{player_id}_z'])
+        break
+    data_csv = pd.DataFrame.from_dict(dict_output, orient='index', columns=columns)
+    data_csv.to_csv("P0001_shooting_alt.csv", index=False)
     return None
 
 convert_to_tab("C:\\Users\\User\\OneDrive\\Desktop\\DS Practice\\Sport\\SPL-Open-Data-Rahul\\basketball\\freethrow\\data\\P0001")
-
-
-
-
-
-
-    
